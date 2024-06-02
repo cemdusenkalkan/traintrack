df50579f503928e41bd8bcddbd95228a5b2e96b8
miraykksal <122979161+miraykksal@users.noreply.github.com>
2024-06-02 01:52:11 +0000

    contact sayfasında email gönderme özelliği

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,57 +1,90 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 import '../ContactPage.css';
 import phoneImage from '../img/phone.png';
 import mailImage from '../img/mail.png';
-
+import backgroundImage from '../img/BackgroundContactPage.jpg';
 
 const ContactPage = () => {
   const [contactPerson, setContactPerson] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [statusMessage, setStatusMessage] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(contactPerson, email, message);
+
+    const templateParams = {
+      contact_person: contactPerson,
+      email,
+      message,
+    };
+
+    emailjs.send('service_uxv0k0w', 'template_ddxg7du', templateParams, 'hTsaHX1UwXobCY4Qu')
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        setStatusMessage('Message sent successfully!');
+      })
+      .catch((error) => {
+        console.error('FAILED...', error);
+        setStatusMessage('Failed to send message.');
+      });
+
+    setContactPerson('');
+    setEmail('');
+    setMessage('');
   };
 
   return (
-    <div className="contact-container">
-      <div className="contact-content">
-        <div className="contact-info">
-          <h2>Contact us</h2>
-          <div className="info-item">
-          <img src={phoneImage} alt="Phone" />
-            <p>+90 538 543 86 10</p>
+    <div className="page" style={{
+      background: `url(${backgroundImage}) no-repeat center center fixed`,
+      backgroundSize: 'cover'
+    }}>
+      <div className="contact-container">
+        <div className="contact-content row">
+          <div className='col-md-6'>
+            <div className="contact-info m-2">
+              <h2>Contact us</h2>
+              <div className="info-item">
+                <img src={phoneImage} alt="Phone" />
+                <p>+90 538 543 86 10</p>
+              </div>
+              <div className="info-item">
+                <img src={mailImage} alt="Mail" />
+                <p>trainntrackk@gmail.com</p>
+              </div>
+            </div>
           </div>
-          <div className="info-item">
-            <img src={mailImage} alt="Mail" />
-            <p>info@tarintrack.com</p>
+          <div className='col-md-6'>
+
+            <form className="contact-form m-2 mb-4" onSubmit={handleSubmit}>
+              <h2>Send us a message</h2>
+              {statusMessage && <p>{statusMessage}</p>}
+              <input
+                type="text"
+                value={contactPerson}
+                onChange={(e) => setContactPerson(e.target.value)}
+                placeholder="Contact person*"
+                required
+              />
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email*"
+                required
+              />
+              <textarea
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Send us a message*"
+                required
+              />
+              <button type="submit">Send Message</button>
+            </form>
           </div>
+
         </div>
-        <form className="contact-form" onSubmit={handleSubmit}>
-          <h2>Send us a message</h2>
-          <input
-            type="text"
-            value={contactPerson}
-            onChange={(e) => setContactPerson(e.target.value)}
-            placeholder="Contact person*"
-            required
-          />
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email*"
-            required
-          />
-          <textarea
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            placeholder="Send us a message*"
-            required
-          />
-          <button type="submit">Send Message</button>
-        </form>
       </div>
     </div>
   );
